3c4da5e1fcbf4876cb1486a7cf8a32175e515989
Gigabyte <Olga-Rabushko1983>
2022-07-10 01:56:17 +0300

    Test

--- Test/by/itstp/OlgaRabushko/Stage12/model/logic/ChessLogicTest.java
+++ Test/by/itstp/OlgaRabushko/Stage12/model/logic/ChessLogicTest.java
@@ -4,7 +4,18 @@ import static org.junit.Assert.*;
 
 public class ChessLogicTest {
     @Test
-    public void checkRockTest() {
+    public void checkRockTest1() {
+        int x1 = 4;
+        int x2 = 5;
+        int y1 = 4;
+        int y2 = 5;
+        boolean expected = true;
+
+        boolean actual = ChessLogic.checkRock(x1, x2, y1, y2);
+        assertEquals(expected,actual);
+        }
+    @Test
+    public void checkRockTest2() {
         int x1 = 4;
         int x2 = 4;
         int y1 = 4;
@@ -13,7 +24,6 @@ public class ChessLogicTest {
 
         boolean actual = ChessLogic.checkRock(x1, x2, y1, y2);
         assertEquals(expected,actual);
-        }
-    }
+    }}
 
 
